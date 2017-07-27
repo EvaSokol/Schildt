15dbe947c9b08dbb575c4f6e58197e3a4192804c
esokolyanskaya <mask@pisem.net>
2017-07-27 18:38:08 +0300

    Factory pattern added

---
+++ src/my_Patterns/Prototype.java
@@ -0,0 +1,36 @@
+package my_Patterns;
+
+/**
+ * Created by Eva Sokolyanskaya on 27/07/2017.
+ * Pattern Prototype realization.
+ */
+public class Prototype
+{
+  public static void main(String[] args){
+    SpecificObject prototype = new SpecificObject();
+    SpecificObject clone = (SpecificObject)prototype.copy();
+    clone.setType(SpecificObject.Type.FIRST_TYPE);
+  }
+}
+
+interface Copyable {
+  Copyable copy();
+}
+
+class SpecificObject implements Copyable {
+  
+  public enum Type {FIRST_TYPE, SECOND_TYPE}
+  Type type;
+  
+  public Copyable copy() {
+    return new SpecificObject();
+  }
+  
+  public void setType (Type type){
+    this.type = type;
+  }
+  
+//  public void printInfo(){
+//    System.out.println(this.na);
+//  }
+}
