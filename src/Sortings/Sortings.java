3d7648ba1e0f68180f402829ab13ff4376de1ba9
MASK <mask@pisem.net>
2017-12-27 21:50:21 +0200

    Sortings created

--- src/Sortings/Sortings.java
+++ src/Sortings/Sortings.java
@@ -7,26 +7,25 @@ import java.util.Arrays;
  * https://www.youtube.com/watch?v=NWXEtQLwAt4
  */
 public class Sortings {
-    public static void main(String[] args){
+
+    public static void main(String [] args) {
         int[] array = {9, 6, 0, 4, 3, 7, 2, 1, 10, 8, 5};
         System.out.println(Arrays.toString(array));
         System.out.println(Arrays.toString(insertSort(array)));
 
     }
 
-    protected static int[] insertSort(int[] array){
-        for (int i=1; i < array.length; i++){
-            int temp = array[i];
-            int prev = i-i;
+    private static int[] insertSort(int[] array){
+        for(int i=1; i< array.length; i++){
+            int tempElement = array[i];
+            int prevIndex = i-1;
 
-            while(prev >=0 && array[prev] > temp) {
-                array[prev + 1] = array[prev];
-                prev--;
+            while(prevIndex >=0 && array[prevIndex] > tempElement) {
+                array[prevIndex + 1] = array[prevIndex];
+                prevIndex --;
             }
-            array[prev+1] = temp;
+            array[prevIndex + 1] = tempElement;
         }
-
         return array;
-
     }
 }
